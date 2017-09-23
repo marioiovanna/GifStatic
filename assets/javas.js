353d393b88643de2a4635d371cafeee60449342f
marioiovanna <marioiovanna@hotmail.com>
2017-09-23 02:04:45 -0400

    fixed btns

--- assets/javas.js
+++ assets/javas.js
@@ -1,72 +1,74 @@
-
 $(document).ready(function () {
 
 
 //BTN topics prefix
-var topics = ["Bart", "Lisa", "Marge", "Maggie", "Homer"];
-console.log(topics);
+    var topics = ["Bart", "Lisa", "Marge", "Maggie", "Homer"];
+    console.log(topics);
 
 // search gif
-function displayTopicInfo() {
+    function displayTopicInfo() {
 
-    $('#gifs').empty();
+        $('#gifs').empty();
 
-    var topics = $(this).attr("data-name");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        topics + "&api_key=dc6zaTOxFJmzC&limit=4";
-    console.log(topics);
-    console.log(queryURL);
+        var topics = $(this).attr("data-name");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+            topics + "&api_key=dc6zaTOxFJmzC&limit=4";
+        console.log(topics);
+        console.log(queryURL);
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).done(function(response) {
-        var results = response.data;
-        console.log(results);
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).done(function (response) {
+            var results = response.data;
+            console.log(results);
 
-        for (var i = 0; i < results.length; i++) {
-            var imageDiv = $("<div class='item'>");
-            var rating = results[i].rating;
-            var still = results[i].images.original_still.url;
-            var animate = results[i].images.original.url;
-            var p = $("<p>").text("Rating: " + rating);
-            var img = $("<img class='thumbnail' height='400px'>");
+            for (var i = 0; i < results.length; i++) {
+                var imageDiv = $("<div class='item'>");
+                var rating = results[i].rating;
+                var still = results[i].images.original_still.url;
+                var animate = results[i].images.original.url;
+                var p = $("<p>").text("Rating: " + rating);
+                var img = $("<img class='thumbnail' height='400px'>");
 
-            img.attr("src", still);
-            imageDiv.append(p);
-            imageDiv.prepend(img);
+                img.attr("src", still);
+                imageDiv.append(p);
+                imageDiv.prepend(img);
 
-            $("#gifs").prepend(imageDiv);
+                $("#gifs").prepend(imageDiv);
 
-            $(img).on('click', function () {
-                img.attr("src", animate);
-            })
-        }
-    });
-}
+                // animate function click
+                $(img).on('click', function () {
+                    img.attr("src", animate);
+                    $(img).on('click', function () {
+                        img.attr("src", still);
+                    })
+                })
+            }
+        });
+    }
 
 // generate BTNS
-function renderButtons() {
-    $('#result').empty();
-    for (var i = 0; i < topics.length; i++) {
-        var a = $("<button>");
-        a.addClass("topic");
-        a.attr("data-name", topics[i]);
-        a.text(topics[i]);
-        $('#result').append(a);
+    function renderButtons() {
+        $('#result').empty();
+        for (var i = 0; i < topics.length; i++) {
+            var a = $("<button>");
+            a.addClass("topic");
+            a.attr("data-name", topics[i]);
+            a.text(topics[i]);
+            $('#result').append(a);
+        }
     }
-}
 
 //  Submit my BTN
-$("#submit-btn").on("click", function(event) {
-    event.preventDefault();
-    var topic = $("#wordadd").val().trim();
-    topics.push(topic);
+    $("#submit-btn").on("click", function (event) {
+        event.preventDefault();
+        var topic = $("#wordadd").val().trim();
+        topics.push(topic);
+        renderButtons();
+    });
     renderButtons();
-});
-renderButtons();
 
-$(document).on("click", ".topic", displayTopicInfo);
-renderButtons();
-
+    $(document).on("click", ".topic", displayTopicInfo);
+    renderButtons();
 });
